4cd8eb93e01a9bd009d749465e5019906d9552cd
AbeerahShamshad <64810902+AbeerahShamshad@users.noreply.github.com>
2021-06-13 20:25:47 +0500

    Add files via upload

---
+++ All_in_one/All_in_one/dbo.Signin.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Signin]
+	@Name nvarchar(50),
+	@Password nvarchar(50),
+	@ConfirmPassword nvarchar(50),
+	@Email nvarchar(50)
+AS
+	Insert into LoginInfo(Name,Password,ConfirmPassword,Email) values(@Name, @Password,@ConfirmPassword,@Email)
+RETURN 0
